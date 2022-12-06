d355acc24cd9553f753ffdb48d644c80b2bd6bdc
steinmeister3000 <aeeinsteiner@gmail.com>
2022-12-05 20:36:54 -0700

    Update settings.py

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['100.20.75.179', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-100-20-75-179.us-west-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
